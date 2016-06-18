bfbf9a83e32bd31a5f85551b031253300ac8ffdf
Rainer <rainer@websimple.be>
2016-06-18 10:02:57 +0200

    Pas de statut pour les instruments

--- base/mf_gestion_instruments.php
+++ base/mf_gestion_instruments.php
@@ -52,12 +52,10 @@ function mf_gestion_instruments_declarer_tables_objets_sql($tables) {
 			"descriptif"         => "mediumtext NOT NULL DEFAULT ''",
 			"nombre"             => "int(11) NOT NULL DEFAULT 1",
 			"date_creation"      => "datetime NOT NULL DEFAULT '0000-00-00 00:00:00'", 
-			"statut"             => "varchar(20)  DEFAULT '0' NOT NULL", 
 			"maj"                => "TIMESTAMP"
 		),
 		'key' => array(
 			"PRIMARY KEY"        => "id_instrument",
-			"KEY statut"         => "statut", 
 		),
 		'titre' => "titre AS titre, '' AS lang",
 		'date' => "date_creation",
@@ -65,23 +63,6 @@ function mf_gestion_instruments_declarer_tables_objets_sql($tables) {
 		'champs_versionnes' => array('titre', 'mf_id', 'descriptif', 'nombre'),
 		'rechercher_champs' => array("titre" => 8, "mf_id" => 8, "descriptif" => 5),
 		'tables_jointures'  => array(),
-		'statut_textes_instituer' => array(
-			'prepa'    => 'texte_statut_en_cours_redaction',
-			'prop'     => 'texte_statut_propose_evaluation',
-			'publie'   => 'texte_statut_publie',
-			'refuse'   => 'texte_statut_refuse',
-			'poubelle' => 'texte_statut_poubelle',
-		),
-		'statut'=> array(
-			array(
-				'champ'     => 'statut',
-				'publie'    => 'publie',
-				'previsu'   => 'publie,prop,prepa',
-				'post_date' => 'date', 
-				'exception' => array('statut','tout')
-			)
-		),
-		'texte_changer_statut' => 'instrument:texte_changer_statut_instrument', 
 		
 
 	);
